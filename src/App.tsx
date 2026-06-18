2a0bdb089c402d550cb9879126d494d5e0c425ad
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-18 20:20:56 +0200

    feat: Setup Project Structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="text-2xl font-bold">Gym Management App</h1>
+    </div>
+  );
+};
+
+export default App;
